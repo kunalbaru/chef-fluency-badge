af48d2f0d43dff4f9356d7983d557480e2761830
Kunal Baru <kunal@linuxacademy.com>
2018-03-08 07:54:15 +0000

    node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,8 +5,14 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 #Install apache package
+if node['platform_family'] == "redhat"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -14,3 +20,5 @@ service 'apache' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
+include_recipe 'apache:websites'
